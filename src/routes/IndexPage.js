99d23add73ceb01a1bda6805da5ddc7ec5a9df8c
hfpp2012 <hfpp2012@gmail.com>
2018-09-17 19:45:08 +0800

    10_modal

---
+++ src/routes/IndexPage.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { connect } from 'dva';
+import styles from './IndexPage.css';
+// import { Button } from 'antd';
+// import Button from 'antd/lib/button';
+import { Modal, Button } from 'antd';
+
+const { confirm } = Modal;
+
+class App extends React.Component {
+  state = { visible: false }
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  }
+
+  handleOk = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
+
+  handleConfirm = () => {
+    confirm({
+      title: 'Do you Want to delete these items?',
+      content: 'Some descriptions',
+      onOk() {
+        console.log('OK');
+      },
+      onCancel() {
+        console.log('Cancel');
+      },
+    });
+  }
+
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
+
+  render() {
+    return (
+      <div className={styles.normal}>
+        <Button type="primary" onClick={this.showModal}>Open</Button>
+        <Modal
+          title="Basic Modal"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          cancelText={"取消"}
+          onCancel={this.handleCancel}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+        <Button type="primary" onClick={ () => this.handleConfirm() }>click me</Button>
+        <h1 className={styles.title}>Yay! Welcome to dva!</h1>
+        <div className={styles.welcome} />
+        <ul className={styles.list}>
+          <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
+          <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+};
+
+export default connect()(App);
